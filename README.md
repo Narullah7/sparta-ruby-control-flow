62e5a5f68d273b7aa38c0389c6547a96b9ff3664
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2018-01-11 21:52:23 +0000

    fixed errors in ruby file and completed README.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# sparta-ruby-control-flow
+# Sparta ruby control flow
+
+In this folder I went through different loops and methods available in Ruby and presented it in a nice a clear way in the terminal
+
+### Getting results
+
+To get the results in the terminal, while being in the root directory of this folder:
+```
+ruby main.rb
+``` 
--- main.rb
+++ main.rb
@@ -1,26 +1,25 @@
-# conditional = 5
-#
-# if conditional < 10
-#   puts "hello"
-# end
-#
-# if conditional < 10
-#   puts "hello"
-# elsif conditional >4
-#   puts "Something else"
-# else
-#   puts "Something else again"
-# end
-# value =1
-# if(value ==1)
-#   puts "1"
-# else
-#   puts "Not 1"
-# end
+# conditions
+puts "---------  conditions -----------------"
+puts ""
+conditional = 5
 
-# turneries
-# puts value < 10 ? "its true" : "it's false"
-
+if conditional < 10
+  puts "hello"
+elsif conditional >4
+  puts "Something else"
+else
+  puts "Something else again"
+end
+value = 1
+if(value ==1)
+  puts "1"
+else
+  puts "Not 1"
+end
+puts ""
+puts "------ turneries ------"
+puts value < 10 ? "its true" : "it's false"
+puts ""
 #comparisons
 # ==
 # !=
@@ -38,56 +37,56 @@
 # ||
 # !
 
-# loops
+puts "------ loops ------"
+puts ""
+puts " 1) while loops "
 
-# 1) while loops
+i = 0
 
-# i = 0
+while i<5 do
+  puts "while: #{i}"
+  i+=1
+end
+puts ""
+puts "2) do while loops"
+j = 1
 
-# while i<10 do
-#   puts "while: #{i}"
-#   i+=1
-# end
+begin
+  puts "Do while: #{j}"
+  j+=1
+end while j<4
+puts ""
+puts "3) until"
 
-# 2) do while loops
+k=0;
 
-# i = 11
-#
-# begin
-#   puts "Do while: #{i}"
-#   i+=1
-# end while i<10
+until k>5 do
+  puts "until #{k}"
+  k+=1
+end
+puts ""
+puts "4) for loops"
 
-# 3) until
+for i in 0..5
+  puts "For loops are great: #{i}"
+end
+puts ""
+puts "5) each loops"
 
-# i=0;
-#
-# until i>10 do
-#   puts "until #{i}"
-#   i+=1
-# end
+(0..5).each do |i|
+  puts "value of local variables is #{i}"
+end
+puts ""
+# using .map function to reverse words
+more_words = ["one","two","three"]
+reversed_words = more_words.map do |word|
+  word.reverse
+end
 
-# 4) for loops
+p reversed_words
 
-# for i in 0..5
-#   puts "For loops are great: #{i}"
-# end
-
-# 5) each loops
-
-# (0..5).each do |i|
-#   puts "value of local variables is #{i}"
-# end
-
-# more_words = ["one","two","three"]
-# reversed_words = more_words.map do |word|
-#   word.reverse
-# end
-#
-# p reversed_words
-
-
-# reduce looping through
+puts ""
+puts "reduce looping through"
 
 some_numbers = [1,4,5,2,3,1,2,4]
 
